eedf07cdc62e47811284de4db1dcf192a9669381
sheena <sheena@antikode.com>
2026-02-07 10:56:01 +0700

    fix missing create path

--- internal/deck/deck.go
+++ internal/deck/deck.go
@@ -443,7 +443,7 @@ func (r *Resource[T]) RegisterRoutes(app *fiber.App) {
 		return c.SendString(html)
 	})
 
-	app.Get(basePath+"/new", func(c *fiber.Ctx) error {
+	app.Get(basePath+"/create", func(c *fiber.Ctx) error {
 		ctx := templ.InitializeContext(c.UserContext())
 
 		fieldComponents := make([]templ.Component, len(r.Fields))
@@ -720,51 +720,4 @@ func Email(name string) *BaseField {
 		name: name,
 		kind: "email",
 	}
-}
-
-func (f *BaseField) Bind(modelType reflect.Type) BoundField {
-	field, ok := modelType.FieldByNameFunc(func(n string) bool {
-		return strings.EqualFold(n, f.name)
-	})
-	if !ok {
-		panic("field not found: " + f.name)
-	}
-
-	return BoundField{
-		Name:  f.name,
-		Label: f.options.Label,
-		Get: func(model any) any {
-			return reflect.ValueOf(model).Elem().FieldByIndex(field.Index).String()
-		},
-		Set: func(model any, value any) {
-			reflect.ValueOf(model).Elem().FieldByIndex(field.Index).SetString(value.(string))
-		},
-		Render: func(v any) templ.Component {
-			return ui.Input(f.name, f.name, "", f.kind, map[string]string{})
-		},
-	}
-}
-
-type BoundField struct {
-	Name   string
-	Label  string
-	Get    func(any) any
-	Set    func(any, any)
-	Render func(any) templ.Component
-}
-
-type BaseColumn struct {
-	name string
-}
-
-func (c *BaseColumn) Name() string {
-	return c.name
-}
-
-func (c *BaseColumn) Render(value any) string {
-	return fmt.Sprintf("%v", value)
-}
-
-func Col(name string) *BaseColumn {
-	return &BaseColumn{name: name}
 }
